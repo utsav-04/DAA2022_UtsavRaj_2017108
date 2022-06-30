21bd204874cc6027c98e87e1b2e74f09ef1799b0
Utsav Raj <100127451+utsav-04@users.noreply.github.com>
2022-06-30 10:28:49 +0530

    Create Qno_1.cpp

---
+++ Week 6/Qno_1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int comp = 0;
+int merge(int arr[], int l, int mid, int h) {
+    int count = 0;
+    int i = l, j = mid + 1;
+    int temp[1000];
+    int k = 0;
+    while (i <= mid && j <= h) {
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else {
+            temp[k++] = arr[j++];
+            count += mid - i + 1;
+        }
+        comp++;
+    }
+    for (; i <= mid;)
+        temp[k++] = arr[i++];
+    for (; j <= h;)
+        temp[k++] = arr[j++];
+    for (int f = 0; f < k; f++)
+        arr[f + l] = temp[f];
+    return count;
+}
+int merge_sort(int arr[], int l, int h)
+{
+    int inversion = 0;
+    if (l < h)
+    {
+        int mid = l + (h - l) / 2;
+        inversion += merge_sort(arr, l, mid);
+        inversion += merge_sort(arr, mid + 1, h);
+        inversion += merge(arr, l, mid, h);
+    }
+    return inversion;
+}
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        int arr[1000];
+        int prev_comp = comp;
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        int inv = merge_sort(arr, 0, n - 1);
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl
+             << "comparisons = " << comp - prev_comp << endl
+             << "inversions = " << inv << endl;
+    }
+    return 0;
+}
